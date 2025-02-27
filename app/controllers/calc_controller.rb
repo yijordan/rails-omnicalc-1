5e8b96a05f9cbfd4df549228dadfe39e5fca7ab6
yijordan <yij@uchicago.edu>
2025-02-27 05:47:27 +0000

    did payment, fixed sqrt link

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -21,6 +21,14 @@ class CalcController < ApplicationController
     render({ :template => "calc_templates/payment_form"})
   end
   def payment
+    @apr = params.fetch("user_apr").to_f.to_fs(:percentage, { :precision =>4 })
+      @r = params.fetch("user_apr").to_f / 1200
+    @years = params.fetch("user_years").to_i
+      @n = @years * 12
+    @pv = params.fetch("user_pv").to_f.to_fs(:currency)
+    @numerator = @r * params.fetch("user_pv").to_f
+    @denominator = 1 - ((1 + @r) ** (-1 * @n))
+    @payment = @numerator / @denominator
     render({ :template => "calc_templates/payment"})
   end
 
