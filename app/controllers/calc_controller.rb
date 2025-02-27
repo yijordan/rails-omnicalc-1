6e93ad1a164452cf2f29760411ce5d1eb1fbac9d
yijordan <yij@uchicago.edu>
2025-02-27 05:34:44 +0000

    made results pages

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -2,16 +2,29 @@ class CalcController < ApplicationController
   def square_form
     render({ :template => "calc_templates/square_form"})
   end
+  def square
+    render({ :template => "calc_templates/square"})
+  end
 
   def sqrt_form
     render({ :template => "calc_templates/sqrt_form"})
+  end
+  def sqrt
+    render({ :template => "calc_templates/sqrt"})
   end
 
   def payment_form
     render({ :template => "calc_templates/payment_form"})
   end
+  def payment
+    render({ :template => "calc_templates/payment"})
+  end
 
   def random_form
     render({ :template => "calc_templates/random_form"})
   end
+  def random
+    render({ :template => "calc_templates/random"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,15 @@ Rails.application.routes.draw do
   get("/", { :controller => "calc", :action => "square_form"})
 
   get("/square/new", { :controller => "calc", :action => "square_form"})
+  get("/square/results", { :controller => "calc", :action => "square"})
 
   get("/square_root/new", { :controller => "calc", :action => "sqrt_form"})
+  get("/square_root/results", { :controller => "calc", :action => "sqrt"})
 
   get("/payment/new", { :controller => "calc", :action => "payment_form"})
+  get("/payment/results", { :controller => "calc", :action => "payment"})
 
   get("/random/new", { :controller => "calc", :action => "random_form"})
+  get("/random/results", { :controller => "calc", :action => "random"})
 
 end
