856a353b1839b944bbf33835edb37f760ced819e
yijordan <yij@uchicago.edu>
2025-02-27 05:29:13 +0000

    all forms done

--- app/controllers/calc_controller.rb
+++ app/controllers/calc_controller.rb
@@ -2,4 +2,16 @@ class CalcController < ApplicationController
   def square_form
     render({ :template => "calc_templates/square_form"})
   end
+
+  def sqrt_form
+    render({ :template => "calc_templates/sqrt_form"})
+  end
+
+  def payment_form
+    render({ :template => "calc_templates/payment_form"})
+  end
+
+  def random_form
+    render({ :template => "calc_templates/random_form"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   get("/square/new", { :controller => "calc", :action => "square_form"})
 
+  get("/square_root/new", { :controller => "calc", :action => "sqrt_form"})
+
+  get("/payment/new", { :controller => "calc", :action => "payment_form"})
+
+  get("/random/new", { :controller => "calc", :action => "random_form"})
+
 end
